91ffa972a80ed429e5bd1e9d3a803190a69ce054
Murillo Welsi <murillo.welsi@gmail.com>
2024-11-27 22:17:01 +0000

    first commit

---
+++ pages/locators/main_page_locators.py
@@ -0,0 +1,5 @@
+class MainPageLocators:
+    BANNER_TITLE = '.highlight_gXVj'
+    BANNER_SUBTITLE = '.hero__title'
+    LINK = 'Get started'
+    HEADING = 'Installation'
---
+++ pages/page_objects/main_page.py
@@ -0,0 +1,18 @@
+from playwright.sync_api import Page
+
+from pages.locators.main_page_locators import MainPageLocators
+from pages.page_objects.base_page import BasePage
+
+
+class MainPage(BasePage):
+    URL = "/"
+
+    def __init__(self, page: Page) -> None:
+        super().__init__(page)
+        self.banner_title = page.locator(MainPageLocators.BANNER_TITLE)
+        self.banner_subtitle = page.locator(MainPageLocators.BANNER_SUBTITLE)
+        self.link = page.get_by_role("link", name=MainPageLocators.LINK)
+        self.heading = page.get_by_role("heading", name=MainPageLocators.HEADING)
+
+    def click_link(self) -> None:
+        self.link.click()
