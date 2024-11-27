7e07be42cdc2e10f4d955692546858a7eb5de7bb
Murillo Welsi <murillo.welsi@gmail.com>
2024-11-27 22:32:09 +0000

    updated workflow

--- conftest.py
+++ conftest.py
@@ -4,13 +4,13 @@ from playwright.sync_api import Page
 from pages.page_objects.main_page import MainPage
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope='session')
 def browser_context_args(browser_context_args):
     return {
         **browser_context_args,
-        "viewport": {
-            "width": 1920,
-            "height": 1080,
+        'viewport': {
+            'width': 1920,
+            'height': 1080,
         },
     }
 
